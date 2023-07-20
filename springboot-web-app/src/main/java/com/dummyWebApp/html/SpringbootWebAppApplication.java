0dfacfd5d08db828ddeba51e0c4922f30a2903db
Gonçalo Rocha <goncaloluzrocha@gmail.com>
2023-07-20 19:50:08 +0100

    Update SpringbootWebAppApplication.java

--- springboot-web-app/src/main/java/com/dummyWebApp/html/SpringbootWebAppApplication.java
+++ springboot-web-app/src/main/java/com/dummyWebApp/html/SpringbootWebAppApplication.java
@@ -1,3 +1,18 @@
+/**
+This class, "SpringbootWebAppApplication," is an essential component of a web 
+application built using the Spring Boot framework. It resides in the package 
+"com.dummyWebApp.html." The class is annotated with "@SpringBootApplication," 
+which indicates that it serves as the main entry point for the application and 
+includes both configuration and component scanning capabilities. The "main" 
+method is the starting point of the application and serves to launch the Spring 
+Boot application. It throws three types of exceptions: IOException, 
+InterruptedException, and ParseException. Inside the "main" method, it calls "
+SpringApplication.run" to initiate the Spring Boot application, providing the 
+class itself and any command-line arguments passed as input. This class plays 
+a crucial role in enabling the smooth execution of the Spring Boot web application, 
+ensuring that it starts up correctly and handles the necessary configuration and scanning processes.
+**/
+
 package com.dummyWebApp.html;
 
 import org.springframework.boot.SpringApplication;
